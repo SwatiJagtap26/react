21fdac04a89dc101cffe4d5da5aff789b1f2e0d0
Swati <swatijagtap2681998@gmail.com>
2021-07-16 21:28:17 +0530

    initial commit

---
+++ src/component/app.js
@@ -0,0 +1,13 @@
+import Heading from './heading';
+import Banner from './banner';
+
+function App() {
+    return(
+        <>
+        <Heading />
+        <Banner />
+        </>
+    )
+    
+}
+export default App;
---
+++ src/component/banner.js
@@ -0,0 +1,10 @@
+function Banner() {
+    return(<>
+        <h2> here is the banner </h2>
+        <button>add</button>
+        
+</>
+    )
+    
+}
+export default Banner;
